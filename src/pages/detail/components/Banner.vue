d4d9ad4657ad7a5a210f7e17ad790afc3ec43540
tonyhew <916390127@qq.com>
2022-06-15 11:18:50 +0800

    增加画廊

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,56 +1,87 @@
 <template>
-  <div class="banner">
-    <img
-      class="banner-img"
-      src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg"
-    />
-    <div class="banner-info">
-      <div class="banner-title">
-        大连圣亚海洋世界(AAAA景区)
-      </div>
-      <div class="banner-number">
-        <span class="iconfont banner-icon">&#xe860;</span>
-        39
+  <div>
+
+    <div class="banner" @click="handleOpenGallery">
+      <img
+        class="banner-img"
+        src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg"
+      />
+      <div class="banner-info">
+        <div class="banner-title">
+          大连圣亚海洋世界(AAAA景区)
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe860;</span>
+          39
+        </div>
       </div>
     </div>
+
+    <common-gallery
+      :imgs="imgs"
+      @close="handleCloseGallery"
+      v-show="showGallery"
+    ></common-gallery>
   </div>
 </template>
 
 <script>
+import CommonGallery from 'common/gallery/Gallery'
+
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  components: {
+    CommonGallery
+  },
+  data () {
+    return {
+      showGallery: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg',
+        'http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png'
+      ]
+    }
+  },
+  methods: {
+    handleCloseGallery () {
+      this.showGallery = false
+    },
+    handleOpenGallery () {
+      this.showGallery = true
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-  .banner
-    position: relative
-    overflow: hidden
-    height: 0
-    padding-bottom: 55%
-    .banner-img
-      width: 100%
-    .banner-info
-      display: flex
-      position: absolute
-      line-height: .6rem
-      left: 0
-      right: 0
-      bottom: 0
-      color: #fff
-      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
-      .banner-title
-        flex: 1
-        font-size: .32rem
-        padding: 0 .2rem
-      .banner-number
-        height: .32rem
-        line-height: .32rem
-        margin: .14rem .2rem
-        padding: 0 .4rem
-        font-size: .24rem
-        border-radius: .2rem
-        background: rgba(0, 0, 0, .8)
-        .banner-icon
-          font-size: .24rem
+.banner
+  position relative
+  overflow hidden
+  height 0
+  padding-bottom 55%
+  .banner-img
+    width 100%
+  .banner-info
+    display flex
+    position absolute
+    line-height 0.6rem
+    left 0
+    right 0
+    bottom 0
+    color #fff
+    background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+    .banner-title
+      flex 1
+      font-size 0.32rem
+      padding 0 0.2rem
+    .banner-number
+      height 0.32rem
+      line-height 0.32rem
+      margin 0.14rem 0.2rem
+      padding 0 0.4rem
+      font-size 0.24rem
+      border-radius 0.2rem
+      background rgba(0, 0, 0, 0.8)
+      .banner-icon
+        font-size 0.24rem
 </style>
